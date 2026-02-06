29c912eb165723901f130649ab43315c4053dd03
Stormfall13 <stormfall228@gmail.com>
2026-02-06 12:18:38 +0500

    fix fix

--- kvazar-pern-server/index.js
+++ kvazar-pern-server/index.js
@@ -22,7 +22,8 @@ const authMiddleware = require("./middlewares/authMiddleware");
 const app = express();
 
 app.use(cors({
-    origin: "*",
+    // origin: "*",
+    origin: ['https://kvazar-pern-client.vercel.app', 'http://localhost:3000'],
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization"
